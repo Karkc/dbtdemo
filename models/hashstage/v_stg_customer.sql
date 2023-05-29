faa5f4427c801acbea90db375f7ea60da6486266
kar88zeal@gmail.com <kar88zeal@gmail.com>
2023-05-29 07:41:24 +0000

    commit

--- models/hashstage/v_stg_customer.sql
+++ models/hashstage/v_stg_customer.sql
@@ -2,10 +2,14 @@
 
 {%- set yaml_metadata -%}
 source_model: "customer"
+derived_columns:
+    RECORD_SOURCE: "!TPCH"
+    LOAD_DATE: "!12-May-2023"
 {%- endset -%}
 
 {% set metadata_dict = fromyaml(yaml_metadata) %}
 {% set source_model = metadata_dict['source_model'] %}
+{% set derived_columns = metadata_dict['derived_columns'] %}
 
 {{ automate_dv.stage(include_source_columns=true,
                      source_model=source_model,
